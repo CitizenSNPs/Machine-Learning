feb8da8758d55117d6c875023f8cc1c130ba4c4a
DPK <35163005+CitizenSNPs@users.noreply.github.com>
2019-01-02 02:11:00 -0800

    Update Titanic.py

--- Kernels/Titanic.py
+++ Kernels/Titanic.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
@@ -8,23 +7,30 @@ from sklearn.metrics import classification_report, confusion_matrix
 import math
 
 sns.set_style('whitegrid')
+
 train_data = pd.read_csv('C:\\Users\\dkirksey\\Downloads\\Titanic\\train.csv')
 test_data = pd.read_csv('C:\\Users\\dkirksey\\Downloads\\Titanic\\test.csv')
+
 print(train_data.head(5))
 print(train_data.describe())
 print(train_data.info())
 
 sns.heatmap(train_data.drop('Survived',axis=1).corr())
 plt.show()
+
 fig = plt.figure(figsize=(10,10))
 ax=plt.axes()
+
+#age vs survival
 sns.distplot(train_data[train_data['Survived']==1]['Age'].dropna(),ax=ax,color='blue')
 sns.distplot(train_data[train_data['Survived']==0]['Age'].dropna(),ax=ax,color='orange')
 plt.show()
 
+#sex vs survival
 sns.countplot(train_data['Survived'], hue=train_data['Sex'])
 plt.show()
 #a larger percentage of women survived than men
+
 sns.countplot(train_data['Embarked'], hue=train_data['Survived'])
 plt.show() #passenger chance of survival: c>Q>S
 
@@ -37,16 +43,15 @@ plt.show() #passengers with higher fares more likely to survive
 
 sns.countplot(train_data['Pclass'], hue=train_data['Survived'])
 plt.show()
-print(train_data['Ticket'])
+
 
 sns.boxplot(train_data['Pclass'], train_data['Age'], hue=train_data['Survived'])
 plt.show()
-cleaned_data = train_data.drop(['Cabin','PassengerId','Name','Ticket'], axis=1)
 
+cleaned_data = train_data.drop(['Cabin','PassengerId','Name','Ticket'], axis=1)
 cleaned_data['Sex']=pd.get_dummies(cleaned_data['Sex'])
 cleaned_data['Embarked']=pd.get_dummies(cleaned_data['Embarked'])
-cleaned_data['Age'] = pd.Series(cleaned_data['Age'].values)
-print (train_data['Age'].mean())
+
 def impute_age(row):
 	if math.isnan(row['Age']):
 		return cleaned_data['Age'].mean()
@@ -54,7 +59,6 @@ def impute_age(row):
 		return row['Age']
 
 cleaned_data['Age']=cleaned_data.apply(lambda row : impute_age(row),axis=1)
-print (cleaned_data['Age'])
 cleaned_data = cleaned_data.dropna()
 
 X = cleaned_data.drop('Survived', axis=1)
@@ -66,3 +70,5 @@ logreg.fit(X_train, y_train)
 y_pred = logreg.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
+print(logreg.intercept_)
+print (pd.DataFrame(logreg.coef_, index=['Coefficients'], columns=X.columns))
